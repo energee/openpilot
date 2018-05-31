07e81ef70d3c43a5cce194132283ed5eac12bc67
Ted Slesinski <ted_slesinski@epam.com>
2018-05-31 08:36:07 -0400

    Add auto-resume

--- selfdrive/car/honda/carstate.py
+++ selfdrive/car/honda/carstate.py
@@ -195,6 +195,8 @@ class CarState(object):
     self.left_blinker_on = 0
     self.right_blinker_on = 0
 
+    self.stopped = 0
+
     # vEgo kalman filter
     dt = 0.01
     # Q = np.matrix([[10.0, 0.0], [0.0, 100.0]])
@@ -301,6 +303,7 @@ class CarState(object):
 
     self.brake_switch = cp.vl["POWERTRAIN_DATA"]['BRAKE_SWITCH']
     if self.CP.carFingerprint in (CAR.CRV_5G, CAR.ACCORD, CAR.CIVIC_HATCH):
+      self.stopped = cp.vl["ACC_HUD"]['CRUISE_SPEED'] == 252.
       self.cruise_speed_offset = calc_cruise_offset(0, self.v_ego)
       if self.CP.carFingerprint == CAR.CIVIC_HATCH:
         self.brake_switch = cp.vl["POWERTRAIN_DATA"]['BRAKE_SWITCH']
